6aea2cc1702db6dee53555ade7846695acbd32e4
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-06-20 16:57:41 +0530

    Modal layout created

--- 1. REACT_PROJECT/3. ModalApplication/src/component/Modal.js
+++ 1. REACT_PROJECT/3. ModalApplication/src/component/Modal.js
@@ -4,7 +4,18 @@ import { Download } from 'lucide-react';
 function Modal() {
   return (
     <div className='fixed inset-0 bg-black/30 backdrop-blur-sm'>
-       Modal
+       <button>
+        <SquareX/>
+       </button>
+       <h1> Download Free E-book </h1>
+       <p>Want to Learn How to Crack Web Development Interviews Like a Pro!</p>
+       <form>
+            <input type='email' placeholder='Enter Your Email' requried/>
+            <button>
+                <Download/>
+                Download eBook
+            </button>
+       </form>
 
     </div>
   )
