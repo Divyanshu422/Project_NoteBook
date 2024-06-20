ac5bf20889c584f142a9d04c4c808286f512b789
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-06-20 17:13:32 +0530

    Implementing the logic of the modal => show

--- 1. REACT_PROJECT/3. ModalApplication/src/App.js
+++ 1. REACT_PROJECT/3. ModalApplication/src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Modal from './component/Modal';
+import { useState } from 'react';
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className="h-screen gap-6 flex flex-col items-center  bg-[#14161b] text-white">
      <h1 className='text-5xl font-bold mt-4'>Popup Modal</h1>
-     <button className='bg-violet-500 px-4 py-2 rounded-lg text-lg'>Get the eBook</button>
-     <Modal/>
+     <button onClick={()=>setShowModal(true) } className='bg-violet-500 px-4 py-2 rounded-lg text-lg'>Get the eBook</button>
+     {
+      showModal && <Modal/>
+     }
+     
     </div>
   );
 }
