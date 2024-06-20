da3a975df45f00cbced3dc735597719cf73c8a17
Divyanshu Tyagi <divyanshu422@gmail.com>
2024-06-20 17:21:13 +0530

    Implementing the onClose functionality => by passing the function to the prop"

--- 1. REACT_PROJECT/3. ModalApplication/src/App.js
+++ 1. REACT_PROJECT/3. ModalApplication/src/App.js
@@ -5,12 +5,17 @@ import { useState } from 'react';
 
 function App() {
   const [showModal, setShowModal] = useState(false);
+
+  // Function to implement the close of modal
+  function onClose(){
+    setShowModal(false);
+  }
   return (
     <div className="h-screen gap-6 flex flex-col items-center  bg-[#14161b] text-white">
      <h1 className='text-5xl font-bold mt-4'>Popup Modal</h1>
      <button onClick={()=>setShowModal(true) } className='bg-violet-500 px-4 py-2 rounded-lg text-lg'>Get the eBook</button>
      {
-      showModal && <Modal/>
+      showModal && <Modal onClose={onClose}/>
      }
      
     </div>
--- 1. REACT_PROJECT/3. ModalApplication/src/component/Modal.js
+++ 1. REACT_PROJECT/3. ModalApplication/src/component/Modal.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { SquareX } from 'lucide-react';
 import { Download } from 'lucide-react';
-function Modal() {
+function Modal({onClose}) {
+    
   return (
     <div className='fixed inset-0 bg-black/30 backdrop-blur-sm flex justify-center items-center'>
         <div className='mt-10 flex flex-col gap-5 text-white'>
-            <button className='place-self-end'>
+            <button className='place-self-end' onClick ={onClose}>
                 <SquareX size={30}/>
             </button>
             <div className='bg-indigo-600 rounded-md px-20 py-10 flex flex-col gap-5 items-center mx-4'>
